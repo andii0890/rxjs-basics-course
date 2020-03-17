378b6d6e7cf71e59b5d5ff9ad8ae4af90ecee765
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-03-17 13:29:51 -0600

    take

---
+++ take.js
@@ -0,0 +1,19 @@
+const { of } = require('rxjs');
+const { take, map, first } = require('rxjs/operators');
+
+const numbers$ = of(1,2,3,4,5);
+const click$ = fromEvent(document, 'click');
+
+click$.pipe(
+	map(event => ({
+		x: event.clientX,
+		y: event.clientY
+	})),
+
+	//filter, take(1)
+	first(({y}) => y > 200)
+	// take(1)
+	).subscribe({
+		next: console.log,
+		complete: () => console.log('complete!')
+	});
